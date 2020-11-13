fbc70dc1297571af469654d8788ab6b2bcc47861
Ramin RX7 <52785580+Ramin-RX7@users.noreply.github.com>
2020-11-13 16:18:46 +0330

    TEST (RX is added to code and there is no import) (x4 Faster)

--- RX.py
+++ RX.py
@@ -1,3 +1,191 @@
+import os
+import time
+import shutil
+import sys
+import re
+import argparse
+import datetime
+from colored import fg,bg,attr
+class rx:
+    @staticmethod
+    def cls():
+        '''
+        You can use this function if you want to clear the environment.
+        '''
+        if __import__('platform').system() == "Windows":
+            os.system('cls')
+        else:
+            os.system('clear')
+
+    class record:
+        def __init__(self):
+            self.__start= time.time()
+            self.laps=[]
+        def lap(self,save=True, Round=15):     
+            lp= round(time.time()-self.__start,Round)
+            if save: self.laps.append(lp)
+            return lp
+        def reset(self,reset_start= False):
+            self.laps= []
+            if reset_start: self.__start= time.time()
+        def last_lap(self, save=True):
+            if save: self.laps.append(self.lap())
+            return (self.lap(False)-self.laps[-1]) if self.laps else self.lap(False)
+    Record = record
+
+    class Terminal:
+        run = os.system
+        getoutput = __import__('subprocess').getoutput
+        set_title = __import__('win32api').SetConsoleTitle
+        get_title = __import__('win32api').GetConsoleTitle
+    terminal = Terminal
+
+    class style:
+        def __init__(self,text,color='default',BG='black'):
+            try:
+                color= color.lower()
+                BG=BG.lower()
+                #style=style.lower()
+            except:
+                pass
+            if color=='default':
+                color=7 #188
+            self.text= text     
+            self.content= fg(color)+bg(BG)+text+attr(0)
+        def __str__(self):
+            return self.content
+        def __repr__(self):
+            return self.content
+        def __add__(self,other):
+            if type(other)!=rx.style:
+                return self.content+other
+            else:
+                return self.content+other.content
+
+        @staticmethod
+        def print(text='', color='default', BG='default', style=0, end='\n'):
+
+            if color=='default' and BG!='default':  # bg & !clr
+                sys.stdout.write(f'{attr(style)}{bg(BG)}{text}{attr(0)}{end}')
+
+            elif color!='default' and BG=='default':  # !bg & clr
+                sys.stdout.write(f'{attr(style)}{fg(color)}{text}{attr(0)}{end}')
+
+            elif color=='default' and BG=='default':  # !bg & !clr
+                sys.stdout.write(f'{attr(style)}{text}{attr(0)}{end}')
+
+            elif color!='default' and BG!='default':  # bg & clr
+                sys.stdout.write(f'{attr(style)}{bg(BG)}{fg(color)}{text}{attr(0)}{end}')
+
+        @staticmethod
+        def switch(color='default', BG='black', style=0):
+            if color == 'default':
+                color = 7
+            print(f'{attr(style)}{bg(BG)}{fg(color)}', end='')
+
+        @staticmethod
+        def switch_default():
+            print(attr(0), end='')
+        reset = switch_default
+
+        @staticmethod
+        def log_error(text, color='red', BG='default', style='bold'):
+            globals()['style'].print(text, color, BG, style=style)
+    Style = style
+
+
+    class files:
+        rename  = os.rename
+        abspath = os.path.abspath
+        exists  = os.path.exists
+        mdftime = os.path.getmtime
+        move    = shutil.move
+        isfile = os.path.isfile
+        @staticmethod
+        def remove(path,force=False):
+            if os.path.isfile(path):
+                os.remove(path)
+            else:
+                if force: 
+                    import shutil
+                    shutil.rmtree(path)
+                else:
+                    try:
+                        os.rmdir(path)
+                    except OSError:
+                        raise OSError(f"[WinError 145] The directory is not empty: '{path}'" + '\n' + ' '*23 + 
+                                    '(Use force=True as an argument of remove function to remove non-empty directories.)')               
+        @staticmethod
+        def hide(path, mode:bool =True):
+            import win32api, win32con
+            if mode:
+                win32api.SetFileAttributes(path,win32con.FILE_ATTRIBUTE_HIDDEN)
+            else:
+                win32api.SetFileAttributes(path,win32con.FILE_ATTRIBUTE_NORMAL)
+        @staticmethod
+        def read(path):
+            with open(path) as f:
+                FileR = f.read()
+            return FileR
+        @staticmethod
+        def write(file, text='',mode='w'):
+            with open(file, mode=mode) as f:
+                f.write(text)
+
+        @staticmethod
+        def mkdir(path):
+            os.mkdir(path)
+    Files = files
+    read  = files.read
+    write = files.write
+
+
+    class system:
+        chdir = os.chdir
+        accname = os.getlogin
+        device_name = __import__('socket').gethostname
+    System = system
+
+
+
+    class io: 
+        @staticmethod
+        def wait_for_input(prompt,SS:list=[], ignore_case=False):
+            answer= ''
+            try:
+                while not answer:
+                    answer = input(prompt).strip()
+                    if ignore_case:
+                        answer = answer.lower()
+                    if answer and SS:
+                        if not (answer in (SS if not ignore_case else [item.lower() for item in SS])):
+                            rx.style.print('Invalid Input','red')
+                            answer = ''
+            except (EOFError,KeyboardInterrupt):
+                rx.style.print('EXITING...','red')
+                exit()
+            return answer
+
+        @staticmethod
+        def selective_input(prompt,choices,default=None,error=False):
+            while True:
+                inp = input(prompt)
+                if not inp  or  inp not in choices:
+                    if error:
+                        rx.style.print('Invalid input', 'red')
+                    else:
+                        inp = default
+                        break
+                else:
+                    break
+            return inp
+        @staticmethod
+        def yesno_input(prompt,default=None):
+            if default: error=False
+            else:       error=True
+            return rx.io.selective_input(prompt,['y','yes','n','no'],default,error)
+    SF = AF = NF = io
+
 """
 ################################################################################
 # CHARS:  {✓ , ? , > , ! , X}
@@ -116,22 +304,12 @@ r"""
 
 
 
-
-
-
-import sys
-import re
-import time
-import argparse
-import datetime
-import os
-
 __version__ = '1.0.0'
 
 START_TIME = time.time()
 
 
-import RX_SuperLite as rx
+#import RX_SuperLite as rx
 #print(f'ImpRX7 :: {time.time()-START_TIME}','green')
 
 print = rx.style.print
@@ -791,7 +969,7 @@ def Syntax(SOURCE,
             SOURCE[Line_Nom-1] = f'{(indent)*" "}if False:pass'
             for Line,snc in enumerate(SOURCE[Line_Nom-1:LAST_LINE], Line_Nom):
                 if Reg := re.search(r'^(D|d)efault\s*:\s*',snc.strip()):
-                    SOURCE[Line-1] = 'else:'
+                    SOURCE[Line-1] = (indent)*" "+'else:'
                     Default = True
                 SEARCH_VALUE = re.search(r'^(C|c)ase\s+(?P<Nobreak>(N|n)obreak)?(?P<VALUE>.+):\s*', snc.strip())
                 if SEARCH_VALUE:
