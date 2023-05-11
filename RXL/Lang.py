c8b7c192acb824b497e831dcf02c7236408475d1
Ramin RX7 <rawmin.rx@gmail.com>
2023-05-12 01:50:02 +0330

    improve array implementation

--- RXL/Lang.py
+++ RXL/Lang.py
@@ -2,30 +2,31 @@ apply = lambda f,iterable: type(iterable)(__import__("builtins").map(f,iterable)
 
 
 
-_auto = -1
+_auto = None
 class array(list):
-    def __init__(self,__iterable=...,type_=_auto, max_length=_auto):
+    def __init__(self,__iterable=...,type_=_auto, max_length:int=_auto):
         if __iterable is not Ellipsis:
             if type_ == _auto:
                 type_ = type(__iterable[0])
-                for element in __iterable:
-                    if type(element) is not type_:
-                        raise TypeError("Given iterable has wrong type (type(element)!=type_)")
+                for item in __iterable:
+                    if (not isinstance(item, type_)) and (item is not None):
+                        raise TypeError(f"Given iterable has wrong type ({type(item)}!={type_})")
                 self._type = type_
             else:
                 members_types = set(type(t) for t in __iterable)
-                if len(members_types) != 1:
+                if (len(members_types) >= 3):
                     raise TypeError("All array elements must have the same type")
-                if list(members_types)[0] != type_:
-                    raise TypeError("Array with wrong element type is given")
-                self._type = list(members_types)[0]
+                if (len(members_types) == 2) and (type(None) not in members_types):
+                    raise TypeError("All array elements must have the same type")
+                self._type = list(members_types-{type(None)})[0]
 
             if max_length == _auto:
                 max_length = len(__iterable)
-            if len(__iterable) > max_length:
+            elif len(__iterable) > max_length:
                 raise MemoryError("Length of given iterable is more that `_max_length`")
             self._max_length = max_length
-            final_iterable = [item for item in __iterable] + [None for _ in range(max_length-len(__iterable))]
+            final_iterable = ([item for item in __iterable] +
+                              [None for _ in range(max_length-len(__iterable))])
             return super().__init__(final_iterable)
 
         else:
@@ -39,12 +40,25 @@ class array(list):
     def __str__(self) -> str:
         return f"<{super().__str__()[1:-1]}>"
 
-    def append(self,__v):
-        if type(__v) != self._type:
+    def append(self, __v, *, shift=False):
+        if shift:
+            self.shift()
+        if (type(__v) != self._type) and (__v is not None):
             raise TypeError("Attempt to add a value with wrong type to array")
-        if len(self) >= self._max_length:
+        for i in range(len(self)):
+            if self[i] is None:
+                self[i] = __v
+                break
+        else:
             raise MemoryError("Maximum size of the array is reached")
-        super().append(__v)
+
+    def shift(self):
+        for i,item in enumerate(self):
+            if item is None:
+                if self[i:] == [None]*(len(self)-i):
+                    break
+                self.pop(i)
+                super().append(None)
 
 
 def constant(f):
