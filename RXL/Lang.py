9a7e7d541973f5823c1742c7c825c1c94d819bb1
Ramin RX7 <rawmin.rx@gmail.com>
2023-05-20 01:32:46 +0330

    add thread to library

--- RXL/Lang.py
+++ RXL/Lang.py
@@ -1,3 +1,5 @@
+import threading
+
 apply = lambda f,iterable: type(iterable)(__import__("builtins").map(f,iterable))
 
 
@@ -63,12 +65,24 @@ class array(list):
                 super().append(None)
 
 
+
 def constant(f):
     def fset(self, value):
         raise PermissionError
     def fget(self):
         return f()
     return property(fget, fset)
+
+
+
+class modified_thread(threading.Thread):
+    def get_result(self):
+        return {k:v for i,(k,v) in enumerate(self.__dict__.items()) if i >= 13}
+
+    @property
+    def result(self):
+        return self.get_result()
+
 
 
 class Singleton(type):
